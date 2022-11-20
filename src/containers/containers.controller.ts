41a70c2e9431ef8eb8dc69ffacaf175ac8d7404b
SelfhostedPro <info@selfhosted.pro>
2022-11-20 00:43:48 -0800

    formatted

--- src/containers/containers.controller.ts
+++ src/containers/containers.controller.ts
@@ -1,68 +1,67 @@
 import {
-    Controller,
-    Get,
-    InternalServerErrorException,
-    BadRequestException,
-    Param,
+  Controller,
+  Get,
+  InternalServerErrorException,
+  BadRequestException,
+  Param,
 } from '@nestjs/common';
 import { ApiTags, ApiCreatedResponse } from '@nestjs/swagger';
 import { ContainerInfo } from 'dockerode';
-import { ContainerInfoDTO } from './classes'
+import { ContainerInfoDTO } from './classes';
 import { ContainersService } from './containers.service';
+
 @ApiTags('containers')
 @Controller('containers')
 export class ContainersController {
-    constructor(private readonly containersService: ContainersService) { }
+  constructor(private readonly containersService: ContainersService) {}
 
-    @Get()
-    @ApiCreatedResponse({
-        description: 'List all containers.',
-        type: [ContainerInfoDTO],
-    })
-    async getContainers(): Promise<ContainerInfo[]> {
-        try {
-            return await this.containersService.getContainers();
-        } catch (err) {
-            throw new InternalServerErrorException(err.code, {
-                cause: new Error(),
-                description: err.message,
-            });
-        }
+  @Get()
+  @ApiCreatedResponse({
+    description: 'List all containers.',
+    type: [ContainerInfoDTO],
+  })
+  async getContainers(): Promise<ContainerInfo[]> {
+    try {
+      return await this.containersService.getContainers();
+    } catch (err) {
+      throw new InternalServerErrorException(err.code, {
+        cause: new Error(),
+        description: err.message,
+      });
     }
-    @Get(':id')
-    @ApiCreatedResponse({
-        description: 'Get inspect information of one container.',
-        type: ContainerInfoDTO,
-    })
-    async getContainerByName(
-        @Param('id') id: string,
-    ): Promise<ContainerInfo> {
-        try {
-            return await this.containersService.getContainer(id);
-        } catch (err) {
-            throw new InternalServerErrorException(err.code, {
-                cause: new Error(),
-                description: err.message,
-            });
-        }
+  }
+  @Get(':id')
+  @ApiCreatedResponse({
+    description: 'Get inspect information of one container.',
+    type: ContainerInfoDTO,
+  })
+  async getContainerByName(@Param('id') id: string): Promise<ContainerInfo> {
+    try {
+      return await this.containersService.getContainer(id);
+    } catch (err) {
+      throw new InternalServerErrorException(err.code, {
+        cause: new Error(),
+        description: err.message,
+      });
     }
-    @Get(':id/:action')
-    @ApiCreatedResponse({
-        description: 'Get inspect information of one container.',
-        type: String,
-    })
-    async containerAction(
-        @Param('id') id: string,
-        @Param('action') action: string
-    ): Promise<string> {
-        try {
-            await this.containersService.containerAction(id, action);
-            return 'sucess'
-        } catch (err) {
-            throw new BadRequestException(err.message, {
-                cause: new Error(),
-                description: err.message,
-            });
-        }
+  }
+  @Get(':id/:action')
+  @ApiCreatedResponse({
+    description: 'Get inspect information of one container.',
+    type: String,
+  })
+  async containerAction(
+    @Param('id') id: string,
+    @Param('action') action: string,
+  ): Promise<string> {
+    try {
+      await this.containersService.containerAction(id, action);
+      return 'sucess';
+    } catch (err) {
+      throw new BadRequestException(err.message, {
+        cause: new Error(),
+        description: err.message,
+      });
     }
+  }
 }
--- src/containers/containers.service.ts
+++ src/containers/containers.service.ts
@@ -14,25 +14,25 @@ export class ContainersService {
     return docker.getContainer(id).inspect();
   }
   async containerAction(id: string, action: string): Promise<ContainerInfo> {
-    const Docker = require('dockerode')
+    const Docker = require('dockerode');
     const docker = new Docker();
-    switch(action) {
-        case 'start':
-            return await docker.getContainer(id).start()
-        case 'stop':
-            return await docker.getContainer(id).stop()
-        case 'pause':
-            return await docker.getContainer(id).pause()
-        case 'unpause':
-            return await docker.getContainer(id).unpause()
-        case 'kill':
-            return await docker.getContainer(id).kill()
-        case 'remove':
-            return await docker.getContainer(id).remove({force: true})
-        case 'restart':
-            return await docker.getContainer(id).restart()
-        default:
-            throw new Error('Error: Action not found.')
+    switch (action) {
+      case 'start':
+        return await docker.getContainer(id).start();
+      case 'stop':
+        return await docker.getContainer(id).stop();
+      case 'pause':
+        return await docker.getContainer(id).pause();
+      case 'unpause':
+        return await docker.getContainer(id).unpause();
+      case 'kill':
+        return await docker.getContainer(id).kill();
+      case 'remove':
+        return await docker.getContainer(id).remove({ force: true });
+      case 'restart':
+        return await docker.getContainer(id).restart();
+      default:
+        throw new Error('Error: Action not found.');
     }
   }
 }
