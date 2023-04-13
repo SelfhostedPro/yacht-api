53dac529c1e941b674f9cd00c3bc32e0c69b1ca4
SelfhostedPro <info@selfhosted.pro>
2023-04-13 10:16:51 -0700

    working on sse

--- ui/src/components/apps/info/stats.vue
+++ ui/src/components/apps/info/stats.vue
@@ -1,12 +1,11 @@
 <template>
-        <v-tooltip v-if="stats" :text="stats.MemoryPercentage.toFixed(2).toString() + ' %' || null" location="bottom">
+        <v-tooltip v-if="stats" :text="stats.MemoryPercentage + ' %' || null" location="bottom">
                 <template v-slot:activator="{ props }">
                         <v-progress-circular v-bind="props" class="mx-2 float-right" :model-value="stats.MemoryPercentage || 0"
                                 :size="40" color="primary"><v-icon icon="mdi-chip" /></v-progress-circular>
                 </template>
         </v-tooltip>
-        <v-tooltip v-if="stats && stats.CpuUsage !== 'NaN'" :text="stats.CpuUsage.toFixed(2).toString() + ' %' || null"
-                location="bottom">
+        <v-tooltip v-if="stats" :text="stats.CpuUsage + ' %' || null" location="bottom">
                 <template v-slot:activator="{ props }">
                         <v-progress-circular v-bind="props" class="mx-2 float-right" :model-value="stats.CpuUsage || 0"
                                 :size="40" color="blue"><v-icon icon="mdi-memory" /></v-progress-circular>
@@ -16,18 +15,5 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { formatStats } from '@/composables/formatApps'
-import { useEventSource } from '@vueuse/core'
-
-const stats = ref(null)
-const props = defineProps(['app'])
-
-const { eventSource, status } = useEventSource(`/api/containers/${props.app.ShortId}/stats`)
-
-eventSource.value.addEventListener('message', (message) => {
-        if (status.value === 'OPEN') {
-                stats.value = formatStats(JSON.parse(message.data))
-        }
-})
-
+const props = defineProps(['app','stats'])
 </script>
--- ui/src/composables/formatApps.ts
+++ ui/src/composables/formatApps.ts
@@ -6,16 +6,12 @@ export interface ReadableContainerInfo extends ContainerInfo {
     ShortId: string,
     ShortName: string
 }
-export interface BasicStats {
-    MemoryPercentage: number,
-    CpuUsage: number
-}
+
 
 export function formatApps(data) {
     const appList: ReadableContainerInfo[] = []
 
     for (const app in data) {
-        console.log(data[app])
         data[app]['CreatedDate'] = format(new Date(data[app]['Created'] * 1000), 'MM/dd/yyyy')
         data[app]['ShortId'] = data[app]['Id'].substring(0,10)
         data[app]['ShortName'] = data[app]['Names'][0].slice(1)
@@ -30,30 +26,4 @@ export function formatInspect(data:ReadableContainerInfo) {
     data['ShortId'] = data['Id'].substring(0,10)
     data['ShortName'] = data['Name'].slice(1)
     return {...data}
-}
-
-export function formatStats(data: ContainerStats) {
-    const formattedStats: BasicStats = {
-        MemoryPercentage: 0,
-        CpuUsage: 0
-    }
-    formattedStats.MemoryPercentage = formatMemPercent(data)
-    formattedStats.CpuUsage = formatCpuPercent(data)
-    return formattedStats
-}
-
-export function formatCpuPercent(data: ContainerStats) {
-    const cpuDelta = data.cpu_stats.cpu_usage.total_usage - data.precpu_stats.cpu_usage.total_usage
-    const systemCpuDelta = data.cpu_stats.system_cpu_usage - data.precpu_stats.system_cpu_usage
-    return (cpuDelta / systemCpuDelta) * data.cpu_stats.online_cpus * 100.0
-}
-
-export function formatMemPercent(data: ContainerStats) {
-    let usedMemory = 0
-    if (data.memory_stats.stats.cache) {
-        usedMemory = data.memory_stats.usage - data.memory_stats.stats.cache
-    } else {
-        usedMemory = data.memory_stats.usage
-    }
-    return (usedMemory / data.memory_stats.limit) * 100.0
 }
