07dc8dced842522239bc2b1eb4fffb3b51601ba7
SelfhostedPro <info@selfhosted.pro>
2023-04-26 11:45:19 -0700

    working on normalizing container data

--- ui/src/types/apps.ts
+++ ui/src/types/apps.ts
@@ -21,7 +21,85 @@ export interface ReadableContainerStats {
 
 }
 
-export interface OciInfo {
+export interface Container {
+    name: string,
+    id: string,
+    created: Date,
+    status: string,
+    restart: {
+        policy: string,
+        count: number
+    }
+    config: {
+        network: ContainerNetworkSettings,
+        general: ContainerGeneralConfig
+    },
+    mounts?: ContainerMount[],
+    ports?: ContainerPorts[],
+    env?: string[],
+    labels?: {
+        [key: string]: string
+    }
+
+}
+
+export interface ContainerGeneralConfig {
+    hostname: string,
+    tty: boolean,
+    user: string,
+    appArmorProfile: string,
+    platform: string,
+    driver: string,
+    path: string,
+    args: string[],
+    autoRemove: boolean,
+    sysctls: {
+        [key: string]: string|boolean
+    }
+    logConfig: {
+        type: string,
+        config: any,
+    }
+}
+
+export interface ContainerMount {
+    type: string
+    name?: string
+    source: string
+    destination: string
+    driver?: string
+    mode: string
+    rw: boolean
+    propagation: string
+}
+
+export interface ContainerPorts {
+    containerPort: string,
+    hostPort: string,
+    hostIP: string
+}
+
+export interface ContainerNetworkSettings {
+    mode: string,
+    macAddress: string,
+    hairpinmode: boolean,
+    networks: {
+        [key: string]: {
+            networkId: string,
+            gateway: string,
+            ipAddress: string,
+            ipamconfig?: IPAMConfig,
+        }
+    }
+}
+
+export interface IPAMConfig {
+    ipv4Address?: string,
+    ipv6Address?: string,
+    linkLocalIps?: string[]
+}
+
+export interface ContainerOciInfo {
     title?: string,
     description?: string,
     docs?: string,
